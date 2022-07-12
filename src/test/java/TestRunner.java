f9313db9b96618c460213d12c080921a8d5dfed9
Wisnu Munawar <54229493+wisnuwm@users.noreply.github.com>
2022-07-12 10:18:09 +0700

    init branch

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "classpath:Features"
+)
+public class TestRunner {
+}
